48abf2324fb8501578ef75c090e1b5399006731d
realrentao <realrentao@users.noreply.github.com>
2026-06-12 18:22:30 +0800

    Initial commit: NUOVO Espresso 1 interactive HTML course

---
+++ js/ne-audio-player.js
@@ -0,0 +1,6 @@
+/**
+ * NUOVO Espresso 1 - Audio Player (deprecated)
+ * speak() function is now defined in audio/ne-audio-data.js
+ * This file is kept for compatibility only.
+ */
+// All functionality moved to audio/ne-audio-data.js
